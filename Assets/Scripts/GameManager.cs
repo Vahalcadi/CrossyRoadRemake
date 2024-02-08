833b949011653fe64872b5e14a869967ba5735ad
Vahalcadi <davidepetrone04@gmail.com>
2024-02-08 17:17:22 +0100

    adjusted terrain spawn behavior, added button to restart level, changed player from a capsule to a cube

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,24 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     private Vector3 currentPos = new Vector3(0, 0, 0);
 
-    public List<GameObject> gameObjects = new();
+    private List<GameObject> gameObjects = new();
+    private int count;
+    public int terrainLimit;
+    public int InitialTerrainCount;
     public List<TerrainScriptableObj> terrains = new();
+
+
+    public static GameManager Instance;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameObjects.Add(Instantiate(terrains[0].terrain));
-        gameObjects[^1].GetComponent<TerrainScriptableObj>();
+
+        if (Instance != null)
+            Destroy(Instance.gameObject);
+        
+        else
+            Instance = this;
+
+
+        for(int i = 0; i < InitialTerrainCount; i++)
+        {
+            SpawnTerrain();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SpawnTerrain();
+            if (count >= terrainLimit)
+            {
+                Destroy(gameObjects[0].gameObject);
+                gameObjects.RemoveAt(0);
+            }
+
+            count++;
+        }
+    }
+
+    private void SpawnTerrain()
+    {
+        gameObjects.Add(Instantiate(terrains[Random.Range(0, terrains.Count)].terrain, currentPos, Quaternion.identity));
         
+        currentPos.z++;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
     }
 }

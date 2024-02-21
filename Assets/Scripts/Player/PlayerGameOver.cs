4f47485a414617f5394f159e39f81af971d69f02
Davide Petrone <davidepetrone04@gmail.com>
2024-02-21 16:51:50 +0100

    added train, added logs. Logs dont move the player when he is on them

--- Assets/Scripts/Player/PlayerGameOver.cs
+++ Assets/Scripts/Player/PlayerGameOver.cs
@@ -2,14 +2,22 @@ using UnityEngine;
 
 public class PlayerGameOver : MonoBehaviour
 {
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Vehicle"))
+            GameOver();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Vehicle") || collision.gameObject.CompareTag("Water"))
-        {
-            GameManager.Instance.UpdateGamePause();
-            GameManager.Instance.GameOverMenu();
-            GameManager.Instance.IsOverSetTrue();
-            Time.timeScale = 0;
-        }
+        if(collision.gameObject.CompareTag("Water"))
+            GameOver();
+    }
+    private void GameOver()
+    {
+        GameManager.Instance.UpdateGamePause();
+        GameManager.Instance.GameOverMenu();
+        GameManager.Instance.IsOverSetTrue();
+        Time.timeScale = 0;
     }
 }
